685e8ee1181a29200d55e4b5bf83490564f9b54b
xinshengzzy <xinshengzzy@gmail.com>
2019-12-31 09:17:57 +0000

    Automatic uploading. No comments!

---
+++ figures/exp84504/res_for_sw_impl_calc.py
@@ -0,0 +1,28 @@
+from simulators.FlowClassifier import FlowClassifier
+from simulators.AHashFlow import AHashFlow
+from flow_tools import *
+import json
+filename = "/root/traces/CAIDA.equinix-nyc.dirA.20180315-125910.UTC.anon.clean.pcap"
+n_pkts = 1000000
+thresh = 5
+def main(filename, n_pkts, thresh):
+	cls = FlowClassifier(filename, n_pkts)
+	ahf = AHashFlow(filename, n_pkts)
+	hh_are = hh_are_calc(cls.flows, ahf.flows, thresh)
+	hh_f1score = hh_f1score_calc(cls.flows, ahf.flows, thresh)
+	res = dict()
+	res["filename"] = filename
+	res["n_pkts"] = n_pkts
+	res["threshold"] = thresh
+	res["hh_are"] = hh_are
+	res["hh_f1score"] = hh_f1score
+	return res
+
+if "__main__" == __name__:
+	l = []
+	for n_pkts in [1, 5, 10, 15, 20]:
+		n_pkts = n_pkts*1000000
+		res = main(filename, n_pkts, thresh)
+		l.append(res)
+	with open("res_for_sw_impl.json", "w") as f:
+		json.dump(l, f)
