02fe10413b9af8947f0bcd20dba37299ea880e37
Zhao Zongyi <zongyi@zongyi-mac.local>
2020-01-04 11:47:46 +0800

    Automatic uploading. No comments!

--- figures/exp84505/plot.py
+++ figures/exp84505/plot.py
@@ -7,54 +7,48 @@ from matplotlib.patches import Ellipse
 font = {'size':18}
 matplotlib.rc('font', **font)
 
-sw = "res_for_sw_impl.json"
-hw = "res_for_hw_impl.json"
-
 src1 = "./cdf.caida.20180315-125910.json"
 src2 = "./cdf.caida.20180315-130000.json"
 src3 = "./cdf.hgc.20080415000.json"
 src4 = "./cdf.hgc.20080415001.json"
 
+len_max = 30
+
 if __name__ == "__main__":
 	with open(src1, "r") as f:
 		cdf1 = json.load(f)
+		len1 = len(cdf1)
+		if len1 > len_max:
+			len1 = len_max
 	with open(src2, "r") as f:
 		cdf2 = json.load(f)
+		len2 = len(cdf2)
+		if len2 > len_max:
+			len2 = len_max
 	with open(src3, "r") as f:
 		cdf3 = json.load(f)
+		len3 = len(cdf3)
+		if len3 > len_max:
+			len3 = len_max
 	with open(src4, "r") as f:
 		cdf4 = json.load(f)
-	are_sw = []
-	are_hw = []
-	f1score_sw = []
-	f1score_hw = []
-	n_pkts = ["1000000", "5000000", "10000000", "15000000", "20000000"]
-	for key in n_pkts:
-		are_sw.append(res_sw[key]["hh_are"])
-		f1score_sw.append(res_sw[key]["hh_f1score"])
-		are_hw.append(res_hw[key]["hh_are"])
-		f1score_hw.append(res_hw[key]["hh_f1score"])
+		len4 = len(cdf4)
+		if len4 > len_max:
+			len4 = len_max
+
 
 	plt.figure(1)
 #	plt.title("Heavy Hitters ARE")
-	plt.xticks(range(5), ("1M", "5M", "10M", "15M", "20M"))
-	plt.plot(range(5), are_sw, label = "Software", marker = "x")
-	plt.plot(range(5), are_hw, label = "Hardware", marker = "o")
+#	plt.xticks(range(5), ("1M", "5M", "10M", "15M", "20M"))
+	plt.ylim(0, 1.05)
+	plt.plot(range(len1), cdf1[0: len1], label = "CAIDA1", marker = "x", mfc="none")
+	plt.plot(range(len2), cdf2[0: len2], label = "CAIDA2", marker = "s", mfc="none")
+	plt.plot(range(len3), cdf3[0: len3], label = "ISP1", marker = "o", mfc="none")
+	plt.plot(range(len4), cdf4[0: len4], label = "ISP2", marker = "1", mfc="none")
+#	plt.plot(range(5), are_hw, label = "Hardware", marker = "o")
 #	plt.legend(bbox_to_anchor=(0.0, 1.02, 1.0, 0.102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.0)
-	plt.legend(loc = 1)
-	plt.xlabel("Num. of Replayed Packets")
-	plt.ylabel("ARE")
-	plt.savefig("hh_are.pdf", bbox_inches = "tight")
-	plt.savefig("hh_are.png", bbox_inches = "tight")
-
-	plt.figure(2)
-#	plt.title("Heavy Hitters ARE")
-	plt.xticks(range(5), ("1M", "5M", "10M", "15M", "20M"))
-	plt.plot(range(5), f1score_sw, label = "Software", marker = "x")
-	plt.plot(range(5), f1score_hw, label = "Hardware", marker = "o")
-#	plt.legend(bbox_to_anchor=(0.0, 1.02, 1.0, 0.102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.0)
-	plt.legend(loc = 1)
-	plt.xlabel("Num. of Replayed Packets")
-	plt.ylabel("ARE")
-	plt.savefig("hh_are.pdf", bbox_inches = "tight")
-	plt.savefig("hh_are.png", bbox_inches = "tight")
+	plt.legend(loc = 4)
+	plt.xlabel("Interval/sec")
+	plt.ylabel("CDF")
+	plt.savefig("cdf.pdf", bbox_inches = "tight")
+	plt.savefig("cdf.png", bbox_inches = "tight")
