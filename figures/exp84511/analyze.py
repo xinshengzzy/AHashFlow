7318b874d5ca6df05b6c251302b1acc9c9155857
xinshengzzy <xinshengzzy@gmail.com>
2020-01-08 05:20:41 +0000

    Automatic uploading. No comments!

--- figures/exp84511/analyze.py
+++ figures/exp84511/analyze.py
@@ -15,6 +15,7 @@ src = "/root/traces/CAIDA.equinix-nyc.dirA.20180315-130000.UTC.anon.json"
 dst1 = "./caida.13000.dhf.json"
 dst2 = "./caida.13000.ahf.n.2.json"
 dst3 = "./caida.13000.ahf.n.4.json"
+dst4 = "./caida.13000.ahf.n.8.json"
 
 def func(cls, hf):
 	results = dict()
@@ -30,44 +31,57 @@ def func(cls, hf):
 	return results
 
 def calc():
-	'''对数据包进行流归类'''
+#	'''对数据包进行流归类'''
 	cls = FlowClassifier.FlowClassifier(src, TYPE_JSON, n_pkts)
-	'''生成DHashFlow的结果'''
-	print "DHashFlow"
-	results = dict()
-	for gamma in range(1, 11):
-		print "gamma:", gamma
-		DHashFlow.set_gamma(gamma)
-		dhf = DHashFlow.DHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, dhf)
-		results[str(gamma)] = temp
-	with open(dst1, "w") as f:
-		json.dump(results, f)
-
-	'''生成AHashFlow(N=2)的结果'''
-	print "AHashFlow"
-	AHashFlow.set_n(2)
-	results = dict()
-	for gamma in range(2, 11):
-		print "gamma:", gamma
-		AHashFlow.set_gamma(gamma)
-		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, ahf)
-		results[str(gamma)] = temp
-	with open(dst2, "w") as f:
-		json.dump(results, f)
+#	'''生成DHashFlow的结果'''
+#	print "DHashFlow"
+#	results = dict()
+#	for gamma in range(1, 11):
+#		print "gamma:", gamma
+#		DHashFlow.set_gamma(gamma)
+#		dhf = DHashFlow.DHashFlow(src, TYPE_JSON, n_pkts)
+#		temp = func(cls, dhf)
+#		results[str(gamma)] = temp
+#	with open(dst1, "w") as f:
+#		json.dump(results, f)
+#
+#	'''生成AHashFlow(N=2)的结果'''
+#	print "AHashFlow"
+#	AHashFlow.set_n(2)
+#	results = dict()
+#	for gamma in range(2, 11):
+#		print "gamma:", gamma
+#		AHashFlow.set_gamma(gamma)
+#		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
+#		temp = func(cls, ahf)
+#		results[str(gamma)] = temp
+#	with open(dst2, "w") as f:
+#		json.dump(results, f)
+#
+#	'''生成AHashFlow(N=4)的结果'''
+#	print "AHashFlow"
+#	AHashFlow.set_n(4)
+#	results = dict()
+#	for gamma in range(4, 11):
+#		print "gamma:", gamma
+#		AHashFlow.set_gamma(gamma)
+#		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
+#		temp = func(cls, ahf)
+#		results[str(gamma)] = temp
+#	with open(dst3, "w") as f:
+#		json.dump(results, f)
 
 	'''生成AHashFlow(N=4)的结果'''
 	print "AHashFlow"
-	AHashFlow.set_n(4)
+	AHashFlow.set_n(8)
 	results = dict()
-	for gamma in range(4, 11):
+	for gamma in range(8, 11):
 		print "gamma:", gamma
 		AHashFlow.set_gamma(gamma)
 		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
 		temp = func(cls, ahf)
 		results[str(gamma)] = temp
-	with open(dst3, "w") as f:
+	with open(dst4, "w") as f:
 		json.dump(results, f)
 
 if "__main__" == __name__:
