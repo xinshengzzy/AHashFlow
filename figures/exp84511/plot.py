b9a92f6a1ee51c4b65623b3d8b709a8e2d750ebd
Zhao Zongyi <zongyi@zongyi-mac.local>
2020-01-08 08:49:08 +0800

    Automatic uploading. No comments!

--- figures/exp84511/plot.py
+++ figures/exp84511/plot.py
@@ -2,6 +2,7 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.patches import Ellipse
+import numpy as np
 
 font = {'size':18}
 matplotlib.rc('font', **font)
@@ -13,12 +14,13 @@ def func(res, gamma):
 	thresh = ["10", "20", "30", "40", "50", "60"]
 	are = []
 	f1score = []
+	n_promotions = res[gamma]["n_promotions"]
 	for item in thresh:
 		tmp1 = res[gamma]["are"][item]
 		tmp2 = res[gamma]["f1score"][item]
 		are.append(tmp1)
 		f1score.append(tmp2)
-	return are, f1score
+	return are, f1score, n_promotions
 
 if __name__ == "__main__":
 	with open(src1, "r") as f:
@@ -26,12 +28,12 @@ if __name__ == "__main__":
 	with open(src2, "r") as f:
 		res2 = json.load(f)
 	
-	are0, f1score0 = func(res2, "4")
-	are1, f1score1 = func(res2, "5")
-	are2, f1score2 = func(res1, "2")
-	are3, f1score3 = func(res1, "3")
-	are4, f1score4 = func(res1, "4")
-	are5, f1score5 = func(res1, "5")
+	are0, f1score0, n0 = func(res2, "4")
+	are1, f1score1, n1 = func(res2, "5")
+	are2, f1score2, n2 = func(res1, "2")
+	are3, f1score3, n3 = func(res1, "3")
+	are4, f1score4, n4 = func(res1, "4")
+	are5, f1score5, n5 = func(res1, "5")
 
 	thresh = [10, 20, 30, 40, 50, 60]
 	plt.figure(1)
@@ -71,20 +73,23 @@ if __name__ == "__main__":
 	plt.savefig("f1score.pdf", bbox_inches = "tight")
 	plt.savefig("f1score.png", bbox_inches = "tight")
 
-	exit()
-
-	plt.figure(3)
+	fig = plt.figure(3)
+	n_promotions = [n0, n1, n2, n3, n4, n5]
 	for i in range(len(n_promotions)):
 		n_promotions[i] = n_promotions[i]/100000.0
-	print n_promotions
-#	plt.title("Heavy Hitters ARE")
-#	plt.xticks(range(5), ("1M", "5M", "10M", "15M", "20M"))
-	plt.plot(gammas, n_promotions, marker = "x", mfc="none")
-#	plt.plot(range(5), are_hw, label = "Hardware", marker = "o")
-#	plt.legend(bbox_to_anchor=(0.0, 1.02, 1.0, 0.102), loc = 3, ncol = 2, mode = "expand", borderaxespad = 0.0)
-#	plt.legend(loc = 3)
-	plt.xlabel(r"$\gamma$")
+	ax = fig.add_axes([0,0,1,1])
+	ax.set_xlim([0,7])
+	ax.set_ylim([0,10])
+	plt.bar(np.arange(1, 7) - 0.25, n_promotions, color = 'r', width = 0.5)
+	plt.xlabel("Settings")
 	plt.ylabel(r"Num. of Promotions($\times 10^{5}$)")
+	plt.xticks(range(1, 7), ("AHF-4", "AHF-5", "DHF-2", "DHF-3", "DHF-4", "DHF-5"))
+	rects = ax.patches
+	labels = [str(item) for item in n_promotions]
+	for rect, label in zip(rects, labels):
+		height = rect.get_height()
+		ax.text(rect.get_x() + rect.get_width() / 2, height + 0.1, label,
+				ha='center', va='bottom')
 	plt.savefig("npromotions.pdf", bbox_inches = "tight")
 	plt.savefig("npromotions.png", bbox_inches = "tight")
 
--- figures/exp84511/test.py
+++ figures/exp84511/test.py
@@ -1,7 +1,2 @@
-import json
-import sys
-import math
-
-if "__main__" == __name__:
-	x = lambda a, b, c: str(a) + "..." + str(b) + "^^^" + str(c)
-	print x(3, 4, 5)
+l = [str(item) for item in range(10, 101, 10)]
+print l
