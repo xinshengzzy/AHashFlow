f1611a5b21801ad8bff3ef1ec5c5c1050bf75565
Zhao Zongyi <zongyi@zongyi-mac.local>
2020-01-08 16:29:51 +0800

    Automatic uploading. No comments!

--- figures/exp84511/test.py
+++ figures/exp84511/test.py
@@ -1,8 +1,4 @@
-import simulators.AHashFlow as AHashFlow
-from my_constants import *
-
-for n in [2, 4]:
-	for gamma in range(10):
-		AHashFlow.set_n(n)
-		AHashFlow.set_gamma(gamma)
-		ahf = AHashFlow.AHashFlow("./test.txt", TYPE_JSON, -1)
+bands = [[10, 20], [20, 30],
+	[30, 40]]
+for item in bands:
+	print item[0], item[1]
