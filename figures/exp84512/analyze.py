d3ffd8e6be5617725ff66d31c658502840b023b9
xinshengzzy <xinshengzzy@gmail.com>
2020-01-09 12:06:00 +0000

    Automatic uploading. No comments!

--- figures/exp84512/analyze.py
+++ figures/exp84512/analyze.py
@@ -12,91 +12,29 @@ n_pkts = -1
 #n_pkts = 1000
 
 src = "/root/traces/CAIDA.equinix-nyc.dirA.20180315-130000.UTC.anon.json"
-dst1 = "./caida.13000.dhf.json"
-dst2 = "./caida.13000.ahf.n.2.json"
-dst3 = "./caida.13000.ahf.n.4.json"
-dst4 = "./caida.13000.ahf.n.8.json"
-dst5 = "./caida.13000.ahf.n.16.json"
+dst = "./res.caida.13000.json"
 
-def func(cls, hf):
+def func(param, flows):
+	AHashFlow.set_n(param)
+	AHashFlow.set_gamma(param)
+	ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
 	results = dict()
-	results["n_promotions"] = len(hf.records)
-	results["n_exports"] = len(hf.ids)
-	results["are"] = dict()
-	results["f1score"] = dict()
-	for thresh in range(10, 101, 10):
-		are = hh_are_calc(cls.flows, hf.flows, thresh)
-		f1score = hh_f1score_calc(cls.flows, hf.flows, thresh)
-		results["are"][thresh] = are
-		results["f1score"][thresh] = f1score
+	results["n_promotions"] = len(ahf.records)
+	results["n_exports"] = len(ahf.ids)
+	ae = banded_ae_calc(ahf.flows, flows, 0, -1)
+	results["ae"] = ae
 	return results
 
 def calc():
-#	'''对数据包进行流归类'''
+	'''对数据包进行流归类'''
 	cls = FlowClassifier.FlowClassifier(src, TYPE_JSON, n_pkts)
-	'''生成DHashFlow的结果'''
-	print "DHashFlow"
-	results = dict()
-	for gamma in range(11, 21):
-		print "gamma:", gamma
-		DHashFlow.set_gamma(gamma)
-		dhf = DHashFlow.DHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, dhf)
-		results[str(gamma)] = temp
-	with open(dst1, "w") as f:
-		json.dump(results, f)
-
-	'''生成AHashFlow(N=2)的结果'''
-	print "AHashFlow"
-	AHashFlow.set_n(2)
-	results = dict()
-	for gamma in range(11, 21):
-		print "gamma:", gamma
-		AHashFlow.set_gamma(gamma)
-		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, ahf)
-		results[str(gamma)] = temp
-	with open(dst2, "w") as f:
-		json.dump(results, f)
-
-	'''生成AHashFlow(N=4)的结果'''
-	print "AHashFlow"
-	AHashFlow.set_n(4)
-	results = dict()
-	for gamma in range(11, 21):
-		print "gamma:", gamma
-		AHashFlow.set_gamma(gamma)
-		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, ahf)
-		results[str(gamma)] = temp
-	with open(dst3, "w") as f:
-		json.dump(results, f)
-
-	'''生成AHashFlow(N=8)的结果'''
-	print "AHashFlow"
-	AHashFlow.set_n(8)
-	results = dict()
-	for gamma in range(11, 21):
-		print "gamma:", gamma
-		AHashFlow.set_gamma(gamma)
-		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, ahf)
-		results[str(gamma)] = temp
-	with open(dst4, "w") as f:
-		json.dump(results, f)
-
-	'''生成AHashFlow(N=16)的结果'''
-	print "AHashFlow"
-	AHashFlow.set_n(16)
-	results = dict()
-	for gamma in range(16, 21):
-		print "gamma:", gamma
-		AHashFlow.set_gamma(gamma)
-		ahf = AHashFlow.AHashFlow(src, TYPE_JSON, n_pkts)
-		temp = func(cls, ahf)
-		results[str(gamma)] = temp
-	with open(dst5, "w") as f:
-		json.dump(results, f)
+	res = dict()
+	for param in [2, 4, 8, 16, 32]:
+		print "param:", param
+		temp = func(param, cls.flows)
+		res[param] = temp
+	with open(dst, "w") as f:
+   		json.dump(res, f)	   
 
 if "__main__" == __name__:
 	calc()
