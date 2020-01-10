a0cd3f967a27e00389e741b99de43206849197a5
Zhao Zongyi <zongyi@zongyi-mac.local>
2020-01-10 21:01:22 +0800

    Automatic uploading. No comments!

--- figures/exp84513/plot.py
+++ figures/exp84513/plot.py
@@ -9,44 +9,29 @@ matplotlib.rc('font', **font)
 
 src = "./res.caida.13000.json"
 
-def func(res, gamma):
-	thresh = [str(item) for item in range(10, 101, 10)]
-	are = []
-	f1score = []
-	n_promotions = res[gamma]["n_promotions"]
-	for item in thresh:
-		tmp1 = res[gamma]["are"][item]
-		tmp2 = res[gamma]["f1score"][item]
-		are.append(tmp1)
-		f1score.append(tmp2)
-	return are, f1score, n_promotions
-
 if __name__ == "__main__":
 	with open(src, "r") as f:
 		res = json.load(f)
 	Ns = ["8", "16", "32", "64", "128", "256"]
+	n_promotions = []
 	AE1 = []
 	AE2 = []
 	for key in Ns:
+		n_promotions.append(res[key]["n_promotions"])
 		AE1.append(res[key]["ae1"])
 		AE2.append(res[key]["ae2"])
-
-	print "AE1:"
-	print AE1
-	print "AE2:"
-	print AE2
-	exit()
+		print "n:", key, ", npromotions:", res[key]["n_promotions"], ", ae1:", res[key]["ae1"], ", ae2:", res[key]["ae2"]
 
 	fig = plt.figure(1)
 	ax = fig.add_axes([0,0,1,1])
 	ax.set_xlim([0,7])
 #	ax.set_ylim([0,8])
-	plt.bar(np.arange(1, 7) - 0.25, AEs, color = 'r', width = 0.5)
+	plt.bar(np.arange(1, 7) - 0.25, AE2, color = 'r', width = 0.5)
 	plt.xlabel("N")
 	plt.ylabel("AE")
 	plt.xticks(range(1, 7), Ns)
 	rects = ax.patches
-	labels = [str(int(item*100)/100.0) for item in AEs]
+	labels = [str(int(item*100)/100.0) for item in AE2]
 	algs = ["DHF", "DHF", "DHF", "AHF", "AHF", "AHF"]
 	for rect, label in zip(rects, labels):
 		height = rect.get_height()
