53ff74bee6008db042b60599c5767544aedec1b5
xinshengzzy <xinshengzzy@gmail.com>
2020-01-31 18:26:00 +0800

    Automatic uploading. No comments!

--- figures/exp84515/test.py
+++ figures/exp84515/test.py
@@ -1,37 +1,7 @@
-import json
-
-def func(src):
-	with open(src, "r") as f:
-		res = json.load(f)
-
-	print("n_promotions:", res["n_promotions"])
-	for thresh in range(10, 101, 10):
-		ae = res["ae"][str(thresh)]
-		are = res["are"][str(thresh)]
-		f1score = res["f1score"][str(thresh)]
-		print("thresh:%d, ae:%.3f, are:%.3f, f1score:%.3f" % (thresh, ae, are, f1score))
-
-#n = 2
-#gamma = 4
-#print("AHashFlow: n=%d, gamma=%d" % (n, gamma))
-#filename = "./ahf.n.%d.gamma.%d.caida.130000.json" % (n, gamma)
-#func(filename)
-
-n = 1
-gamma = 4
-print("EHashFlow: n=%d, gamma=%d" % (n, gamma))
-filename = "./ehf.n.%d.gamma.%d.caida.130000.json" % (n, gamma)
-func(filename)
-
-n = 1
-gamma = 6
-print("EHashFlow: n=%d, gamma=%d" % (n, gamma))
-filename = "./ehf.n.%d.gamma.%d.caida.130000.json" % (n, gamma)
-func(filename)
-
-n = 1
-gamma = 8
-print("EHashFlow: n=%d, gamma=%d" % (n, gamma))
-filename = "./ehf.n.%d.gamma.%d.caida.130000.json" % (n, gamma)
-func(filename)
-
+a = 4
+b = 3
+c = 9
+d = 10
+for i in range(a + b
+	+ c + d):
+	print("i=%d" % i)
--- figures/exp84516/test.py
+++
@@ -1,4 +0,0 @@
-from simulators.ZipfTrafficGenerator import Zipf流量生成器
-
-流量生成器 = Zipf流量生成器(80000, 20, 1.2, 100000)
-print("包序列长度：%d" % len(流量生成器.包序列))
